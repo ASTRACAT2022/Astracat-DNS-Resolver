d938047532c1168ea8b19c25d3ccbdf6135a2ea6
vovazyablov2010 <vovazyablov2010@gmail.com>
2025-08-21 04:05:11 +0300

    Add all files

---
+++ complete.go
@@ -0,0 +1,13 @@
+package main
+
+// func main() {
+// 	r := NewResolver(WithCache(10000))
+// 	rrs, err := r.Resolve("google.com", "TXT")
+// 	if err != nil {
+// 		fmt.Println("Error:", err)
+// 		return
+// 	}
+// 	for _, rr := range rrs {
+// 		fmt.Println(rr.String())
+// 	}
+// }
